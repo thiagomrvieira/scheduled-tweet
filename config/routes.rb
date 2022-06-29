137f69fbeda9df3f34741fba150d2189efcf6fb2
Thiago Vieira <tmaciel@digital-works.com>
2022-06-29 18:50:07 +0100

    Creating a sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   # get "/", to: "main#index"
   # same as
   root to: "main#index"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
 end
