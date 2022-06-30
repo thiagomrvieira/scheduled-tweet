87d0448c69472da4a3b792733e334cc963a561f6
Thiago Vieira <tmaciel@digital-works.com>
2022-06-30 19:51:02 +0100

    Login with Session Cookies

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,9 @@
 class MainController < ApplicationController
     def index
-        flash.now[:notice] = "Logged in successfully"
-        flash.now[:alert]  = "Login Fail"
+        if session[:user_id]
+            # @user = User.find(session[:user_id]) Throw an error if doesn't find
+            @user = User.find_by(id: session[:user_id])
+
+        end
     end
 end
