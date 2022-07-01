c0ac0505d18c777668077d6a9a0ac473c61bb057
Thiago Vieira <tmaciel@digital-works.com>
2022-07-01 12:23:34 +0100

    Accessing the Current User

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,4 @@
 class MainController < ApplicationController
     def index
-        if session[:user_id]
-            # @user = User.find(session[:user_id]) Throw an error if doesn't find
-            @user = User.find_by(id: session[:user_id])
-
-        end
     end
 end
