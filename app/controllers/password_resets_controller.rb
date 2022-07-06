84aa4f40439e2fd2a73dfe1fdfea8d8f815cd424
Thiago Vieira <tmaciel@digital-works.com>
2022-07-06 11:39:22 +0100

    Forgot Your Password

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,16 @@
+class PasswordResetsController < ApplicationController
+    def new
+    end
+
+    def create
+        @user = User.find_by(email: params[:email])
+
+        if @user.present?
+            #send email
+            PasswordMailer.with(user: @user).reset.deliver_later
+        end
+
+        redirect_to root_path, notice: "If an account with that email was found, we have sent an email"
+    end
+
+end
