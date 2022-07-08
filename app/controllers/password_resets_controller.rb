cdf6845e7856452e2da5b9fabc5bb4a7a2832b98
Thiago Vieira <tmaciel@digital-works.com>
2022-07-08 15:22:05 +0100

    Reset PassworPassword Reset Updated Token Mailer

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,13 +4,31 @@ class PasswordResetsController < ApplicationController
 
     def create
         @user = User.find_by(email: params[:email])
-
         if @user.present?
             #send email
             PasswordMailer.with(user: @user).reset.deliver_later
         end
-
         redirect_to root_path, notice: "If an account with that email was found, we have sent an email"
     end
 
+    def edit
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+        redirect_to sign_in_path, alert: "Your token has expired. Please try again"
+    end
+
+    def update
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+        if @user.update(password_params)
+            redirect_to sign_in_path, notice: "Your password was reset sucessfully. Please sign in."
+        else
+            render :edit
+        end
+    end
+
+    private 
+    
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
+    end
 end
